96b613b6819a5a7993f32d5cc75617747b39ab4f
MicheleDap85 <studio.micheledapollo@gmail.com>
2026-02-17 18:31:33 +0100

    cancellato componente sbagliato in home
    Removed unnecessary HTML boilerplate from home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
     <x-layout>
 
     @if(session()->has('emailSent'))
@@ -42,7 +33,3 @@
     </div>
 
 </x-layout>
-
-   
-</body>
-</html>
